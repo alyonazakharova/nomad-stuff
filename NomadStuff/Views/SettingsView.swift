6541e870bf231fb511f2047308e776120f6ed251
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2025-01-26 18:26:56 +0100

    Simplified settings view

--- NomadStuff/Views/SettingsView.swift
+++ NomadStuff/Views/SettingsView.swift
@@ -6,32 +6,34 @@ struct SettingsView: View {
     @State private var citizenshipCountry: Country?
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Citizenship") {
-                    HStack {
-                        Text("Country")
-                        Spacer()
-                        Text(citizenshipCountry?.name ?? "Not selected")
-                            .foregroundColor(.gray)
-                    }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        showingCitizenshipSelection = true
-                    }
+        Form {
+            Section("Citizenship") {
+                HStack {
+                    Text("Country")
+                    Spacer()
+                    Text(citizenshipCountry?.name ?? "Not selected")
+                        .foregroundColor(.gray)
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    showingCitizenshipSelection = true
                 }
             }
-            .navigationTitle("Settings")
-            .sheet(isPresented: $showingCitizenshipSelection) {
-                CitizenshipSelectionView(isPresented: $showingCitizenshipSelection)
-            }
-            .onAppear {
+        }
+//        .scrollContentBackground(.hidden)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            showingCitizenshipSelection = true
+        }
+        .sheet(isPresented: $showingCitizenshipSelection) {
+            CitizenshipSelectionView(isPresented: $showingCitizenshipSelection)
+        }
+        .onAppear {
+            citizenshipCountry = CountryManager.shared.loadSavedCountry()
+        }
+        .onChange(of: showingCitizenshipSelection) { oldValue, newValue in
+            if !newValue {
                 citizenshipCountry = CountryManager.shared.loadSavedCountry()
-            }
-            .onChange(of: showingCitizenshipSelection) { oldValue, newValue in
-                if !newValue {
-                    citizenshipCountry = CountryManager.shared.loadSavedCountry()
-                }
             }
         }
     }
